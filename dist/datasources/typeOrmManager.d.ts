be76955edb85a941c12c16514f5b4d4faa6b716c
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-06-30 10:35:50 -0300

    Atualização de dependências e testes

--- dist/datasources/typeOrmManager.d.ts
+++
@@ -1,9 +0,0 @@
-import { Connection } from 'typeorm';
-declare class TypeOrmManager {
-    protected static entities: any[];
-    static connect(config: any): Promise<Connection>;
-    static close(name: string): Promise<void>;
-    static getConnection(name: string): Connection;
-    static wait(config: any): Promise<void>;
-}
-export default TypeOrmManager;
--- dist/enums/joinType.d.ts
+++
@@ -1,2 +0,0 @@
-declare type JoinType = 'innerJoin' | 'innerJoinAndSelect' | 'leftJoin' | 'leftJoinAndSelect';
-export default JoinType;
